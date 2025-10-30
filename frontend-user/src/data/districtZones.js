130fc99dc209ed7fcf689913bbad9d78e4411b25
Yasara Siriwardhana <ryasara2000@gmail.com>
2025-10-30 09:38:04 +0530

    008

---
+++ frontend-user/src/data/districtZones.js
@@ -0,0 +1,14 @@
+export const districtZones = {
+  Kalutara: [
+    "Kalutara Town", "Horana", "Panadura", "Wadduwa", "Matugama",
+    "Agalawatta", "Bandaragama", "Beruwala", "Bulathsinhala", "Dharga Town"
+  ],
+  Gampaha: [
+    "Gampaha Town", "Negombo", "Ja-Ela", "Wattala", "Kelaniya",
+    "Ragama", "Minuwangoda", "Divulapitiya", "Dompe", "Ganemulla"
+  ],
+  Colombo: [
+    "Colombo 1", "Colombo 2", "Colombo 3", "Dehiwala", "Moratuwa",
+    "Nugegoda", "Battaramulla", "Piliyandala", "Thimbirigasyaya", "Kotte"
+  ]
+};
